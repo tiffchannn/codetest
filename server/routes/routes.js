f45634e108d0700d6776a7e33c81adbf9de40640
tiffchannn <tiffany.yi.chan@gmail.com>
2021-07-21 15:39:25 -0700

    Modified route for updating song

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -5,6 +5,6 @@ module.exports = function (app) {
     app.post('/api/song', controller.createSong);
     app.get('/api/song', controller.findAllSongs);
     app.get('/api/song/:id', controller.findOneSong);
-    app.put('api/song/:id', controller.updateSong);
+    app.put('/api/song/:id', controller.updateSong);
     app.delete('/api/song/:id', controller.deleteSong)
 }
