2923d1a74b41e7b9a0440b632fc9952c5cb8c922
tiffchannn <tiffany.yi.chan@gmail.com>
2021-07-22 16:51:06 -0700

    Added option to resolve deprecation warnings for findOneAndUpdate

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useFindAndModify: false
 })
     .then(() => console.log("Established a connection to the database"))
     .catch(err => console.log("Something went wrong when connecting to the database", err));
