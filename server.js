8c897f537e55d55e9b1a3beaf8c98b1e0963e7bf
tiffchannn <tiffany.yi.chan@gmail.com>
2021-07-22 10:28:48 -0700

    Updated port connection within server.js

--- server.js
+++ server.js
@@ -20,13 +20,15 @@ app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
 });
 
-app.listen(8000, () => {
-    console.log("Listening at Port 8000")
-})
+// app.listen(8000, () => {
+//     console.log("Listening at Port 8000")
+// })
 
 // Port connection for Heroku deployment
-// let port = process.env.PORT;
-// if (port == null || port == "") {
-//     port = 8000;
-// }
-// app.listen(port);
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 8000;
+}
+app.listen(port, () => {
+    console.log("Listening at Port 8000")
+})
