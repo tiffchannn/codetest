581dd83fb195788de19a45d046a9ed775faa1336
tiffchannn <tiffany.yi.chan@gmail.com>
2021-07-22 09:24:55 -0700

    Refactored hard coded connection string to Heroku config variable

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-mongoose.connect("mongodb://localhost/songdb", {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
